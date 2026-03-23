9a915f1cca453c81c94ef8a4aac345a52a0e97af
Stefan Schramm <mail@stefanschramm.net>
2026-03-23 08:13:16 +0100

    Remove obsolete declarations

--- winmapviewer.cpp
+++ winmapviewer.cpp
@@ -29,11 +29,8 @@ HWND hwndStatus;
 HWND hwndMap;
 HWND hWnd;
 
-ATOM MyRegisterClass(HINSTANCE hInstance);
-BOOL InitInstance(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK About(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK MapWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 void changeStyle(int styleIdentifier);
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
